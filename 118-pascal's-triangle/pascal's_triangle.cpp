fc3d728cd63933f7fa641cbcf3b14baa9d71f6df
swastik <swastikchouhan7@gmail.com>
2024-09-08 14:56:34 +0530

    Time : 0 ms || Memory : 7.98 MB

--- 118-pascal's-triangle/pascal's_triangle.cpp
+++ 118-pascal's-triangle/pascal's_triangle.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>pt;
-        for(int i=1;i<=numRows;i++){
+        for(int row=1;row<=numRows;row++){
             long long ans=1;
             vector<int> ansRow;
-            ansRow.push_back(1);
-            for(int j=1;j<i;j++){
-                ans=ans*(i-j);
-                ans/=j;
+            ansRow.push_back(ans);
+            for(int col=1;col<row;col++){
+                ans=ans*(row-col);
+                ans/=col;
                 ansRow.push_back(ans);
             }
             pt.push_back(ansRow); 
@@ -17,3 +17,20 @@ public:
         return pt;
     }
 };
+
+/* 
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> pt;
+        for(int i=0;i<numRows;i++){
+            vector<int> v(i+1,1);
+            for(int j=1;j<i;j++){
+                v[j]=pt[i-1][j]+pt[i-1][j-1];
+            }
+            pt.push_back(v);
+        }
+        return pt;
+    }
+};
+*/
