1543d28c6dba16cf42f4fb70f3be2c493de9fc8b
swastik <swastikchouhan7@gmail.com>
2024-09-10 02:49:18 +0530

    Time : 83ms || Memory : 70.4 MB

---
+++ 53_maximum_subarray/maximumSubarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0,res=INT_MIN;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            res=max(res,sum);
+            if(sum<0) sum=0;
+        }
+        return res;
+    }
+};
