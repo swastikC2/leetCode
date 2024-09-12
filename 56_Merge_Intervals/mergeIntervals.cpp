0bbee2fd1020564a6903419cc3b06e77833a5fc3
swastik <swastikchouhan7@gmail.com>
2024-09-13 00:39:40 +0530

    Time : 19ms | Memory : 22.99MB

--- 56_Merge_Intervals/mergeIntervals.cpp
+++ 56_Merge_Intervals/mergeIntervals.cpp
@@ -4,13 +4,12 @@ public:
         int n=intervals.size();
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> res;
-        for(int i=0;i<n;i++){
-            if(res.empty() || intervals[i][0]>res.back()[1]){
-                res.push_back(intervals[i]);
+        res.push_back(intervals[0]);
+        for(int i=1;i<n;i++){
+            if(res.back()[1]>=intervals[i][0]){
+                res.back()[1]=max(res.back()[1],intervals[i][1]);
             }
-            else {
-                res.back()[1]= max(res.back()[1],intervals[i][1]);
-            }
+            else res.push_back(intervals[i]);
         }
         return res;
     }
