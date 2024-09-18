b8355a6568e93eacb146f224c5b03ad97f770686
swastik <swastikchouhan7@gmail.com>
2024-09-18 19:36:36 +0530

    Time : 11ms | Memory : 20.7 MB

---
+++ 268_Missing_Number/missingNumber.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int res = n*(n+1)/2; //sumOfnNaturalNumbers
+        for(int i=0;i<n;i++) res-=nums[i];
+        return res;
+    }
+};
