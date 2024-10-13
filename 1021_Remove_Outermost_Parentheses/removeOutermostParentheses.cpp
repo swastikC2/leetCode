1be5c16c671d615442f9624aadd3d306e9c44808
swastik <swastikchouhan7@gmail.com>
2024-10-14 00:29:59 +0530

    Time : 0ms | Memory : 8.2 MB

---
+++ 1021_Remove_Outermost_Parentheses/removeOutermostParentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string res;
+        int balance=0;
+        for(auto c : s){
+            if(c=='('){
+                if(balance>0) res+=c;
+                balance++;
+            }
+            else{
+                balance--;
+                if(balance>0) res+=c;
+            }
+        }
+        return res;
+    }
+};
