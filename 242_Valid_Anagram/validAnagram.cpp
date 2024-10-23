f91cc7802980f5671051fa6cbb4c7acdb6673443
swastik <swastikchouhan7@gmail.com>
2024-10-24 03:32:42 +0530

    Time : 0ms | Memory : 9.8 MB

---
+++ 242_Valid_Anagram/validAnagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        unordered_map<char,int>mpp;
+        for(int i=0;i<s.length();i++) mpp[s[i]]++;
+        for(int i=0;i<t.length();i++) mpp[t[i]]--;
+        for(auto i=mpp.begin();i!=mpp.end();i++){
+            if(i->second!=0) return false;
+        }
+        return true;
+    }
+};
