5510888f470ecb9d824a06806b1313ce513b767f
swastik <swastikchouhan7@gmail.com>
2024-11-09 04:18:21 +0530

    Time : 0ms | Memory : 16.62 MB

---
+++ 152_Maximum_Product_Subarray/maximumProductSubarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int res = INT_MIN;
+        int prod=1;
+
+        for(int i=0;i<nums.size();i++){
+            prod*=nums[i];
+            res=max(res,prod);
+            if(prod==0){
+                prod=1;
+            }
+        }
+        prod=1;
+        for(int i=nums.size()-1;i>0;i--){
+            prod*=nums[i];
+            res=max(res,prod);
+            if(prod==0){
+                prod=1;
+            }
+        }
+        return res;
+    }
+};
