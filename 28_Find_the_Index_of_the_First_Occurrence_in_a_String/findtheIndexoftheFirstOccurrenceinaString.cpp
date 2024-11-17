921a5e0b1b7c758edf729be0859f4408cc9fd977
swastik <swastikchouhan7@gmail.com>
2024-11-17 16:07:56 +0530

    Time : 0ms : Memory : 8.3 MB

---
+++ 28_Find_the_Index_of_the_First_Occurrence_in_a_String/findtheIndexoftheFirstOccurrenceinaString.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int m = haystack.size(),n=needle.size();
+        if(m<n){
+            return -1;
+        }
+        for(int i=0;i<=m-n;i++){
+            if(haystack.substr(i,n)==needle){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
